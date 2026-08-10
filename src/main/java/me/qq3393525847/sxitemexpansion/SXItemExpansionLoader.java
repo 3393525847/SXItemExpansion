ed6f4aeb596e8195f0f9b76f1fcf5363b60cd0a6
3393525847 <154670262+3393525847@users.noreply.github.com>
2026-08-10 18:47:35 +0800

    Remove SXItemExpansionLoader class
    Remove unused SXItemExpansionLoader class and its imports.

--- src/main/java/me/qq3393525847/sxitemexpansion/SXItemExpansionLoader.java
+++ src/main/java/me/qq3393525847/sxitemexpansion/SXItemExpansionLoader.java
@@ -1,12 +1,1 @@
-package me.qq3393525847.sxitemexpansion;
 
-import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
-import io.papermc.paper.plugin.loader.PluginLoader;
-
-class SXItemExpansionLoader implements PluginLoader {
-
-    @Override
-    public void classloader(final PluginClasspathBuilder builder) {
-        // Add dynamically loaded libraries here
-    }
-}
